6d5c87c0826fa12a75d8d7ee1a6bc6bee127bd19
Depiliem <davew1726@gmail.com>
2026-02-06 20:32:02 +0700

    kentut

--- SIC/Assets/Scripts/Laser.cs
+++ SIC/Assets/Scripts/Laser.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using UnityEngine.SceneManagement;
 
 public class Laser : MonoBehaviour
 {
     public float lifeTime = 2f;
-    
+
     void Start()
     {
         Destroy(gameObject, lifeTime);
-        
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Panggil sistem nyawa nanti
-            Destroy(other.gameObject);
+            KillPlayer(other.gameObject);
         }
+    }
+
+    void KillPlayer(GameObject player)
+    {
+        Destroy(player);
+
+        // Reset scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
